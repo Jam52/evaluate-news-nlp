bfa3df21e7c9166e3f73242675fdcc8949f6413d
Jam52 <jamie.sajdak@gmail.com>
2020-05-11 13:10:26 -0400

    adding unit tests

--- __tests__/checkIsUrl.test.js
+++ __tests__/checkIsUrl.test.js
@@ -1,7 +1,7 @@
 import {is_url} from '../src/client/js/nameChecker.js';
 import {addHttp} from '../src/client/js/formHandler.js';
 
-//unit test for URL unput
+//unit test for URL input from nameChecker
 test("testing result of URL input", () => {
     expect(is_url('https://www.abc123.com')).toBeTruthy();
     expect(is_url('.www.346/23@')).toBeFalsy();
@@ -9,8 +9,11 @@ test("testing result of URL input", () => {
 }); 
 
 
+//unit test on adding http:// to a url without one
 test("if adds http://", () => {
     const url = "www.url.com"
     expect(addHttp(url).toBe("http://www.url.com"));
+    const url2 = "http://www.url.com"
+    expect(addHttp(url2).toBe("http://www.url.com"));
 })
 
